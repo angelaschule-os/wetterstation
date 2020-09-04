a33753dd72159a6c021f6442ea18297f2c3b0123
BjoernSchilberg <BjoernSchilberg@users.noreply.github.com>
2020-09-04 20:42:35 +0000

    Added csv header

--- Python/serial2csv.py
+++ Python/serial2csv.py
@@ -10,6 +10,12 @@ with open('telegramm.serial', 'r') as reader:
 file = open('telegramm.csv', 'w+', newline ='') 
   
 # writing the data into the file 
-with file:     
+with file:
+    header = ['time_decimal', 'sensor1', 'sensor2', 'sensor3', 'sensor4', 'sensor5', 'sensor6', 'sensor7', 'sensor8', 'sensor9', 'sensor10', 'sensor11', 'sensor12', 'sensor13', 'sensor14', 'date', 'time']
     write = csv.writer(file) 
+    writer = csv.DictWriter(file, fieldnames = header) 
+      
+    # writing data row-wise into the csv file 
+    writer.writeheader()     
+    
     write.writerows(datalist) 
