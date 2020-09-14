84284d9064c0b4944080ef6fb2540de544759335
Bjoern Schilberg <bjoern.schilberg@angelaschule-osnabrueck.net>
2020-09-14 22:47:58 +0200

    Fixed printing empty line

--- Python/thies.py
+++ Python/thies.py
@@ -10,29 +10,35 @@ from sys import stdout
 # https://pyserial.readthedocs.io/en/latest/pyserial_api.html#serial.Serial
 # See docs/Datalogger MeteoLOG TDL14.pdf page 28.
 dmm = Serial(
-    port='/dev/ttyUSB0', 
-    baudrate=9600, 
-    bytesize=SEVENBITS, 
-    parity=PARITY_EVEN, 
-    stopbits=STOPBITS_ONE, 
-    timeout=0.5,                      # in Sekunden
-    xonxoff=False, 
-    rtscts=False, 
-    write_timeout=1,                # in Sekunden
-    dsrdtr=False, 
+    port='/dev/ttyUSB0',
+    baudrate=9600,
+    bytesize=SEVENBITS,
+    parity=PARITY_EVEN,
+    stopbits=STOPBITS_ONE,
+    xonxoff=False,
+    rtscts=False,
+    dsrdtr=False,
     inter_byte_timeout=None
 )
 # https://pyserial.readthedocs.io/en/latest/pyserial_api.html#serial.Serial.rts
 dmm.rts = False
 # https://pyserial.readthedocs.io/en/latest/pyserial_api.html#serial.Serial.dtr
 dmm.dtr = True
+# Clear the queue so that data doesn't overlap and create erroneous data points
+dmm.flushInput()
 
 while True:
-    # https://pyserial.readthedocs.io/en/latest/pyserial_api.html#serial.Serial.readline
-    # Lines are concluded with "CR LF". See docs/Datalogger MeteoLOG TDL14.pdf page 20.
-    # https://developer.mozilla.org/de/docs/Glossary/CRLF
-    s = dmm.readline()
-    print(s)
-    # You want unbuffered output whenever you want to ensure that the output
-    # has been written before continuing. 
-    stdout.flush()
+    try:
+        # https://pyserial.readthedocs.io/en/latest/pyserial_api.html#serial.Serial.readline
+        # Lines are concluded with "CR LF". See docs/Datalogger MeteoLOG TDL14.pdf page 20.
+        # https://developer.mozilla.org/de/docs/Glossary/CRLF
+        #print(dmm.readline())
+        ser_bytes = dmm.readline()
+        decoded_bytes = str(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
+        print(decoded_bytes)
+        # You want unbuffered output whenever you want to ensure that the output
+        # has been written before continuing.
+        stdout.flush()
+    except:
+        print("Keyboard Interrupt")
+        break
