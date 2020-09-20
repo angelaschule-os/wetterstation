13bdc0e97203e2aa0e568b35935bb91dd85e3ef5
Bjoern Schilberg <bjoern.schilberg@angelaschule-osnabrueck.net>
2020-09-20 22:01:30 +0200

    Added go example

---
+++ Go/go.mod
@@ -0,0 +1,8 @@
+module github.com/angeschule-os/wetterstation/Go
+
+go 1.15
+
+require (
+	github.com/jacobsa/go-serial v0.0.0-20180131005756-15cf729a72d4
+	golang.org/x/sys v0.0.0-20200918174421-af09f7315aff // indirect
+)
---
+++ Go/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+
+	"github.com/jacobsa/go-serial/serial"
+)
+
+func main() {
+	options := serial.OpenOptions{
+		PortName:        "/dev/ttyUSB0",
+		BaudRate:        9600,
+		DataBits:        7,
+		StopBits:        1,
+		MinimumReadSize: 4,
+	}
+	serialPort, err := serial.Open(options)
+	if err != nil {
+		log.Fatalf("serial.Open: %v", err)
+	}
+	defer serialPort.Close()
+	reader := bufio.NewReader(serialPort)
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
